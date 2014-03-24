1c18f00ed4ed0f2f1dec622afe7787c7aa57e0c8
Paul Bilnoski <bilnoski@tamu.edu>
2014-03-24 16:47:29 -0500

    Copy e.t.t.db.postgresql from svn [30070]; renamed from e.t.t.db.postgres

---
+++ bundles/edu.tamu.tcat.db.postgresql/lib/readme.txt
@@ -0,0 +1,2 @@
+The PostgreSQL source zip is here because the 'org.postgresql' binary library is provided
+as an OSGi bundle, but not with a source bundle.
---
+++ bundles/edu.tamu.tcat.db.postgresql/src/edu/tamu/tcat/db/postgresql/PostgreSqlDataSourceFactory.java
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * Copyright © 2007-14, All Rights Reserved.
+ * Texas Center for Applied Technology
+ * Texas A&M Engineering Experiment Station
+ * The Texas A&M University System
+ * College Station, Texas, USA 77843
+ *
+ * Use is granted only to authorized licensee.
+ * Proprietary information, not for redistribution.
+ ******************************************************************************/
+
+package edu.tamu.tcat.db.postgresql;
+
+import java.sql.Driver;
+import java.util.Properties;
+
+import edu.tamu.tcat.db.core.AbstractDataSourceFactory;
+import edu.tamu.tcat.db.core.DataSourceException;
+
+public class PostgreSqlDataSourceFactory extends AbstractDataSourceFactory
+{
+    protected Driver driver = null;
+
+    public PostgreSqlPropertiesBuilder getPropertiesBuilder()
+    {
+        return new PostgreSqlPropertiesBuilder();
+    }
+
+    @Override
+    protected String getConnectionUrl(Properties parameters) throws DataSourceException
+    {
+        return getConnectionUrl(parameters.getProperty(PostgreSqlPropertiesBuilder.HOST),
+                                PostgreSqlPropertiesBuilder.getPort(parameters),
+                                parameters.getProperty(PostgreSqlPropertiesBuilder.DATABASE));
+    }
+
+    @Override
+    protected Properties getConnectionProperties(Properties parameters)
+    {
+        Properties prop = new Properties(parameters);
+        prop.remove(PostgreSqlPropertiesBuilder.HOST);
+        prop.remove(PostgreSqlPropertiesBuilder.DATABASE);
+        prop.remove(PostgreSqlPropertiesBuilder.PORT);
+
+        return prop;
+    }
+
+    protected String getConnectionUrl(String server, int port, String database) throws DataSourceException
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("jdbc:postgresql://");
+
+        if (server == null)
+        {
+            throw new DataSourceException("Could not construct database URL. No host specified");
+        }
+        sb.append(server);
+
+        if (port >= 0)
+        {
+            sb.append(":").append(port);
+        }
+
+        if (database == null)
+        {
+            throw new DataSourceException("Could not construct database URL. No database specified");
+        }
+
+        sb.append("/").append(database);
+        return sb.toString();
+    }
+
+    @Override
+    protected Driver getDriver() throws DataSourceException
+    {
+        if (driver == null)
+            driver = new org.postgresql.Driver();
+        return driver;
+    }
+}
