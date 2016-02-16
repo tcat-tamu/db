2c965a012155baa4e62c613ef8879f2d9d112283
Paul Bilnoski <bilnoski@tamu.edu>
2016-02-16 15:45:12 -0600

    Add entity helper utilities

---
+++ bundles/edu.tamu.tcat.db.postgresql/src/edu/tamu/tcat/db/postgresql/PostgreSqlEntityHelper.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2014 Texas A&M Engineering Experiment Station
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.tamu.tcat.db.postgresql;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * A set of utilities to help build database entities in PostgreSQL, such as databases and schemas.
+ */
+public class PostgreSqlEntityHelper
+{
+   private static final Logger debug = Logger.getLogger(PostgreSqlEntityHelper.class.getName());
+
+   /**
+    * @param connDefaultDb A connection to the db server on the database ""
+    * @param targetDatabase The name of the database to create
+    */
+   public static void createDatabaseIfNotExists(Connection connDefaultDb, String targetDatabase)
+   {
+      String sqlCheck = "SELECT EXISTS (SELECT * FROM pg_catalog.pg_database WHERE datname = ?)";
+      try (PreparedStatement existsCheck = connDefaultDb.prepareStatement(sqlCheck))
+      {
+         existsCheck.setString(1, targetDatabase);
+         try (ResultSet results = existsCheck.executeQuery())
+         {
+            if (!results.next())
+            {
+               debug.log(Level.WARNING, "Failed testing for database existence ["+sqlCheck+"]");
+               return;
+            }
+
+            boolean exists = results.getBoolean(1);
+            if (exists)
+               return;
+         }
+      }
+      catch (Exception e)
+      {
+         debug.log(Level.SEVERE, "Failed checking for existence of database ["+targetDatabase+"] with sql ["+sqlCheck+"]", e);
+         // don't try to create if exist check failed
+         return;
+      }
+
+      debug.log(Level.INFO, "Database [" + targetDatabase + "] not found, creating it now");
+      String sqlCreate = "CREATE DATABASE \"" + targetDatabase + "\"";
+      try (PreparedStatement create = connDefaultDb.prepareStatement(sqlCreate))
+      {
+         create.executeUpdate();
+      }
+      catch (Exception e)
+      {
+         debug.log(Level.SEVERE, "Failed creating database ["+targetDatabase+"] with sql ["+sqlCreate+"]", e);
+         return;
+      }
+   }
+
+   /**
+    * Create the "postgis" extension on the database for the given connection. The extension module
+    * must already be installed on the database server, and this will ensure it is available in the database.
+    *
+    * @param conn
+    */
+   public static void createExtensionPostGis(Connection conn)
+   {
+      debug.log(Level.INFO, "Installing PostGIS extensions");
+
+      try (PreparedStatement install = conn.prepareStatement("CREATE EXTENSION postgis"))
+      {
+         install.executeUpdate();
+      }
+      catch (Exception e)
+      {
+         debug.log(Level.WARNING, "Cannot create PostGIS extension, the package may need to be installed on the db server", e);
+      }
+   }
+
+   //NOTE: if these SQL statements fail, things are likely still okay; if the db is in trouble, using the schema later will fail
+   public static void createSchemaIfNotExists(Connection conn, String targetSchemaName)
+   {
+      String sqlCheck = "SELECT EXISTS(SELECT schema_name FROM information_schema.schemata WHERE schema_name = ?)";
+      try (PreparedStatement existsCheck = conn.prepareStatement(sqlCheck))
+      {
+         existsCheck.setString(1, targetSchemaName);
+         try (ResultSet results = existsCheck.executeQuery())
+         {
+            if (!results.next())
+            {
+               debug.log(Level.WARNING, "Failed testing for schema existence ["+sqlCheck+"]");
+               return;
+            }
+
+            boolean exists = results.getBoolean(1);
+            // schema already exists, exit early
+            if (exists)
+               return;
+         }
+      }
+      catch (Exception e)
+      {
+         debug.log(Level.SEVERE, "Failed checking for existence of schema ["+targetSchemaName+"] with sql ["+sqlCheck+"]", e);
+         // don't try to create if exist check failed
+         return;
+      }
+
+      String sqlCreate = "CREATE SCHEMA \"" + targetSchemaName + "\"";
+      try (PreparedStatement create = conn.prepareStatement(sqlCreate))
+      {
+         debug.log(Level.INFO, "Schema [" + targetSchemaName + "] not found, creating it now");
+         create.executeUpdate();
+      }
+      catch (Exception e)
+      {
+         debug.log(Level.SEVERE, "Failed creating schema ["+targetSchemaName+"] with sql ["+sqlCreate+"]", e);
+      }
+   }
+}
